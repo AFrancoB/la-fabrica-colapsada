348c5f04ec390c266a4f6bbc14c167ea65c068bf
Diego Villaseñor <diego.vid.eco@gmail.com>
2024-04-17 08:38:05 -0600

    Improve desktop styles and mobile UX

--- helpers.js
+++ helpers.js
@@ -2,6 +2,7 @@
 function setupFlashNotice () {
     const flashNotice = document.getElementById("flash-notice");
     const buttons = document.querySelectorAll(".boton");
+    const displayArea = document.getElementById("displayArea");
     if(flashNotice && buttons?.length > 0) {
         buttons.forEach((button) => {
             button.addEventListener("click", () => {
@@ -13,7 +14,9 @@ function setupFlashNotice () {
             });
         });
     }
+    flashNotice.addEventListener("click", () => {
+        displayArea.scrollIntoView({ behavior: "smooth" });
+    })
 }
-
 
 setupFlashNotice()
